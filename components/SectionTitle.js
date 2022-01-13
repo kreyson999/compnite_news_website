0312fe8a9e1c30124b9fb8b20d75f0bf01e90bf1
kreyson999 <mlynekjakub05@gmail.com>
2022-01-13 21:05:31 +0100

    small changes

--- components/SectionTitle.js
+++ components/SectionTitle.js
@@ -1,7 +1,7 @@
 const PostsHeaderTitle = ({text, border, category}) => {
   if (border) {
     return (
-      <h3 className="text-2xl md:text-2xl font-bold mb-3 border-b-2 pb-2 border-gray-600">{text} {category ? <span className="text-green-900">{category}</span> : null}</h3>
+      <h3 className="text-2xl md:text-2xl font-bold mb-3 border-b-2 pb-2">{text} {category ? <span className="text-green-900">{category}</span> : null}</h3>
     );
   } else {
     return (
