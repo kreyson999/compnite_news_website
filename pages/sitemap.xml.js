4660a61e3e71c29441da71e9ab95b49d822427de
kreyson999 <mlynekjakub05@gmail.com>
2022-01-26 15:40:07 +0100

    trying to fix vercel sitemap bug

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -6,7 +6,7 @@ const Sitemap = () => {};
 
 export const getServerSideProps = async ({ res }) => {
   const baseUrl = {
-    development: "http://localhost:3000",
+    development: "pages",
     production: "./",
   }[process.env.NODE_ENV];
 
